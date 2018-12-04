e6ff359d24cb0c057bd24f643b800a7762af11f2
BandsWithLegends <adamosimpson21@gmail.com>
2018-12-03 21:24:20 -0800

    making stuff

---
+++ src/teamBuilder/TeamBuilder.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './TeamBuilder.css'
+
+class TeamBuilder extends Component{
+ render(){
+   return(<div>Something here</div>)
+ }
+}
+
+export default TeamBuilder;
